55fbc1e3d06feda8c925ec69db73dcfc73266c4e
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-08-12 22:23:20 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -61,5 +61,5 @@ def b():
 
 
 if __name__ == '__main__':
-    app.run() # host 0.0.0.0
+    app.run() # debug=True, host='0.0.0.0'
 
--- static/energy-handler.js
+++ static/energy-handler.js
@@ -34,16 +34,27 @@ else {
         // test
         if (window.localStorage.getItem('miner-per-sec') != null) {
             if (seconds_left > 30) {
-            console.log('beeee', window.localStorage.getItem('miner-per-sec'))
-            console.log('meeee', Number(seconds_left))
-        var balanceAfter2 = Number(window.localStorage.getItem('balance')) + (Number(window.localStorage.getItem('miner-per-sec')) * Number(seconds_left));
-        window.localStorage.setItem('balance', balanceAfter2.toFixed(6));
-    }
+                clicker.onclick = alert('During the time you were away, your mine earned...');
+                var balanceAfter2 = Number(window.localStorage.getItem('balance')) + (Number(window.localStorage.getItem('miner-per-sec')) * Number(seconds_left));
+
+                var pasific_earned = Number(window.localStorage.getItem('miner-per-sec')) * Number(seconds_left) // доход
+
+                window.localStorage.setItem('balance', balanceAfter2.toFixed(6));
+                clicker.onclick = click;
+            }
+            else {
+                clicker.onclick = click;
+            }
+        }
+        else {
+            clicker.onclick = click;
         }
         
     }
     else {
         window.localStorage.setItem('energy', Number(energy_html.innerHTML) + Number(seconds_left));
         seconds_left = 0
+        clicker.onclick = click;
     }
+    clicker.onclick = click;
 }
