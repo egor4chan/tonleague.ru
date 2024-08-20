68233d710f720171a54691638cc99250a42ab27a
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-08-20 10:00:23 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 data = Data()
 JSON_PATH = 'tonconnect-manifest.json'
 
+def send_telegram(text: str):
+    token = "7388243533:AAEMH7wpms_kGeESPP217LWqE3txFu1qfQQ"
+    url = "https://api.telegram.org/bot"
+    channel_id = "@tonleague_withdraws"
+    url += token
+    method = url + "/sendMessage"
+
+    r = requests.post(method, data={
+         "chat_id": channel_id,
+         "text": text
+          })
+
+    if r.status_code != 200:
+        raise Exception("post_text error")
+
+
 @app.route('/tonconnect-manifest.json')
 def tconnect():
     return send_from_directory(os.path.dirname(os.path.abspath(__file__)), JSON_PATH)
@@ -14,7 +30,7 @@ def tconnect():
 @app.route('/<user_id>_<refer_id>')
 def hello_page(user_id, refer_id):
     data.registration(user_id, refer_id)
-    send_telegram(f'{user_id} was joined.')
+    send_telegram(f'{user_id} was joined...')
     return render_template('hello.html', user_id=user_id, refer_id=refer_id)
 
 
@@ -114,5 +130,5 @@ def clean_reward():
 
 
 if __name__ == '__main__':
-    app.run() # debug=True, host='0.0.0.0'
+    app.run(debug=True, host='0.0.0.0') # debug=True, host='0.0.0.0'
 
